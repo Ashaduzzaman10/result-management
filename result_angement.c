ea13b79777db0db62d055922e5158595831a2e4b
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-21 00:25:04 +0600

    done

---
+++ result_angement.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char name[20];
+    int roll;
+    float result;
+} Student;
+
+int main() {
+    int numberOfStudent;
+
+    // here input the  number of students
+    printf("how much student in the class : ");
+    scanf("%d", &numberOfStudent);
+
+    // memory allocation for student using struc using an array
+    Student *students = (Student *)malloc(numberOfStudent * sizeof(Student));
+
+    if (students == NULL) {
+        printf("Memory allocation failed.\n"); // handle the error
+        return 1;
+    }
+
+    // here we provide student information
+    for (int i = 0; i < numberOfStudent; i++) {
+        printf("provide some information for student %d:\n", i + 1);
+
+        // Input name and checking input validation 
+        akash:
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        if((students[0].name[0]>='a' && students[0].name[0]<='z') || (students[0].name[0]>='A' && students[0].name[0]<='Z')) ;
+        else
+        {
+            goto akash;
+        }
+
+        // Input roll
+        akash1:
+        printf("Roll: ");
+        char check[3];
+        scanf("%s", &check);
+        int size = strlen(check);
+        int done = 1;
+        for(int j=0; j<size; j++)
+        {
+            if(check[j]>='0' && check[j]<='9') ;
+            else
+            {
+                done = 0;
+                break;
+            }
+        }
+        if(done)
+        {
+            int roll = 0;
+            for(int j=0; j<size; j++)
+            {
+                int a = check[j]-'0';
+                if(j==0) roll = a;
+                else
+                {
+                    roll *= 10;
+                    roll += a;
+                }
+            }
+            students[i].roll = roll;
+        }
+        else
+        {
+            goto akash1;
+        }
+
+
+        // Input result
+        printf("Result: ");
+        if (scanf("%f", &students[i].result) != 1) {
+            printf("Invalid input for result. Please enter a number.\n"); //error handling
+            return 1;
+        }
+    }
+
+
+    // creating file for those student based on th condition
+    FILE *bonusFile = fopen("bonus_students.txt", "w"); // toper are store in bonus file
+    FILE *normalFile = fopen("normal_students.txt", "w"); // normal  are store in normal file
+
+    if (bonusFile == NULL || normalFile == NULL) {
+        printf("Error creating files.\n"); // handling error for creating file
+        return 1;
+    }
+
+
+    // based on the result here i categorize the student
+    for (int i = 0; i < numberOfStudent; i++) {
+        if (students[i].result >= 3.80) {
+            fprintf(bonusFile, "Name: %s, Roll: %d, Result: %.2f\n", students[i].name, students[i].roll, students[i].result);
+        } else {
+            fprintf(normalFile, "Name: %s, Roll: %d, Result: %.2f\n", students[i].name, students[i].roll, students[i].result);
+        }
+    }
+
+    // Close files
+    fclose(bonusFile);
+    fclose(normalFile);
+
+    // Free allocated memory
+    free(students);
+
+    printf("Student information categorized successfully based on their result .\n");
+    
+
+    return 0;
+}
+// and finally closed the program
